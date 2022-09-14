ded432d51fa3ac41c2607fb0a578556153519657
soclassique <soclassique@yahoo.com>
2022-09-14 08:26:19 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* print_sign - print the sign of a number
+*
+* @n: a number
+*
+* Return: 1 and print + if n is greater than 0
+* 0 if n is zero
+* -1 and print - if n is less than zero
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n < 0)
+{
+_putchar(45);
+return (-1);
+}
+else
+{
+_putchar(48);
+return (0);
+}
+}
