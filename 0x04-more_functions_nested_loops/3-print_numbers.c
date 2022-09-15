b0aff9fe7c563f1a703270a19d380706f53a65e5
soclassique <soclassique@yahoo.com>
2022-09-15 13:27:39 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,7 +9,7 @@ void print_numbers(void)
 char j;
 for (j = '0'; j <= '9'; j++)
 {
-_putchar(j);
+putchar(j);
 }
-_putchar('\n');
+putchar('\n');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,6 +4,7 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void positive_or_negative(int i);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
