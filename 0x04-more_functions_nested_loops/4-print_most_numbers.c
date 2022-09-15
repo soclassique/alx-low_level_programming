e6a8ce704dd9d75774bc1721d2742acc571eb99c
soclassique <soclassique@yahoo.com>
2022-09-15 13:40:05 -0700

    Print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_most_numbers - print digit character
+*
+* Return: void
+*/
+void print_most_numbers(void)
+{
+char h;
+for (h = '0'; h <= '9'; h++)
+{
+if (!(h == '2' || h == '4'))
+putchar(h);
+}
+putchar('\n');
+}
