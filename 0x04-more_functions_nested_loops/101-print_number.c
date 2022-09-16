876943b2ecac02bb20a0874b06e95a016cd61231
soclassique <soclassique@yahoo.com>
2022-09-16 11:35:26 -0700

    101 print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,41 +1,65 @@
-#include <stdio.h>
-void factor_prime(long);
+#include "main.h"
+
 /**
-* main - entry point
-*
-* Return: Always 0 (Success)
-*/
-int main(void)
+ * print_number - prints an integer
+ * @n: integer to print
+ * Return: void
+ */
+
+void print_number(int n)
 {
-factor_prime(1231952);
-return (0);
-}
-/**
-* factor_prime - prints the prime factors of a number
-* @n: number
-*/
-void factor_prime(long n)
-{
-int i, x, count, res;
-for (i = n - 1; i > 0; i--)
-{
-count = 0;
-x = 1;
-while (x <= i)
-{
-if (i % x == 0 && i > 1)
-count++;
-x++;
-}
-if (count <= 2)
-{
-if (n % i == 0)
-{
-res = i;
-printf("%d ", res);
-break;
-}
-}
-}
-printf("\n");
-}
+	unsigned int i = n;
+	
+	if (n < 0)
+	{
+		putchar(45);
+		i = -i;
+	}
+
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+
+	putchar((i % 10) + '0');
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
