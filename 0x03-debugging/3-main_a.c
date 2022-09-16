973c1ad714fa75cba245776ecb0af05a15862035
soclassique <soclassique@yahoo.com>
2022-09-16 10:44:13 -0700

    Leap year

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* main - takes a date and prints how many days are left in the year, taking
-* leap years into account
-* Return: 0
+ * main - takes a date and prints how many days are left in the year, taking
+ * leap years into account
+ * Return: 0
 */
 int main(void)
 {
