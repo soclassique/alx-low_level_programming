757e4ec8a01d3041d37540f26bed3a742bf23ee0
soclassique <soclassique@yahoo.com>
2022-09-16 08:44:34 -0700

    main file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
 * main - function that prints if integer is positive or negative
 * Description: main file
-* Return: 0
+* Return: always 0 (Success).
 */
 int main(void)
 {
