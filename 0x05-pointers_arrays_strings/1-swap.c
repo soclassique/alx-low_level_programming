00cf0f27551563ae404d35207476f31ce95e7a32
soclassique <soclassique@yahoo.com>
2022-09-19 12:14:24 -0700

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps two integer
+ * @a: frist integer
+ * @b: second integer
+ * Return: nothing
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int j;
+	
+	j = *a;
+	*a = *b;
+	*b = j;
+}
