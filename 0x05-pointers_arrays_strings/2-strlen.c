00c43827ad09c7f0aa92b28ecefb8f35c4cc128d
soclassique <soclassique@yahoo.com>
2022-09-19 14:07:34 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,13 @@
 #include "main.h"
-
 /**
- * _strlen - returns the lenght of string
- * @s: string to find length
- * Return: length of string
- */
-
+* _strlen - returns the lenght of string
+* @s: string to find length
+* Return: length of string
+*/
 int _strlen(char *s)
 {
-	int m = 0;
-	
-	while (*(s + m))
-		m++;
-	return (m);
+int m = 0;
+while (*(s + m))
+m++;
+return (m);
 }
