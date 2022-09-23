da147b1eaa8014d86d9025b275fe6b16628cbc5e
soclassique <soclassique@yahoo.com>
2022-09-22 19:42:57 -0700

    print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,13 +8,13 @@ void print_number(int n)
 unsigned int tens, digit, positive = n;
 double t_beg = 1;
 if (n == 0)
-putchar('0');
+_putchar('0');
 else
 {
 if (n < 0)
 {
 positive = n * -1;
-putchar('-');
+_putchar('-');
 }
 while (t_beg <= positive)
 t_beg *= 10;
@@ -22,7 +22,7 @@ tens = t_beg / 10;
 while (tens >= 1)
 {
 digit = positive / tens;
-putchar(digit + '0');
+_putchar(digit + '0');
 positive = (positive - (tens * digit));
 tens /= 10;
 }
