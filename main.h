81a7201d0c01c9d6639bde23ae80db376136884d
Sogbeye <sogbeye.oloju@gmail.com>
2022-10-04 16:02:27 -0700

    create array

--- main.h
+++ main.h
@@ -1,13 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
-
-int _putchar(char c);
+int _putchar(char);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 
 #endif
